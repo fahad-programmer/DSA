73a74d152931a149d923af8e178f3f0f4e4c2593
Fahad Malik <unexpectedprogrammer@gmail.com>
2023-05-04 17:51:15 +0500

    changes made to binary algorithm to help understand the algorithm better

--- binary_search_algorithm.py
+++ binary_search_algorithm.py
@@ -1,39 +1,20 @@
-def binary_search_algorithm(arr, target):
-    """
-    *Args (arr, target)
-
-     -> arr -> The array that is sorted in which we have to search
-     -> target -> The target which we have to find
-
-    *Return
-     -> The index of the target 
-
-    *Important*
-     ->  Must have a time complexity of O(Log N)
-
-    """
-
+def binary_search(arr, target):
     left = 0
     right = len(arr) - 1
-
+    
     while left <= right:
-
-        
-        
         middle = (left + right) // 2
-
-        print(f"The middle is {middle}, The Left Is {left}, And The Right Is {right} And Arr Item Is {arr[middle]}")
-
+        print("middle:", middle)
+        
         if arr[middle] == target:
+            print("target found at index", middle)
             return middle
         elif arr[middle] < target:
+            print("target is in right half of array")
             left = middle + 1
         else:
+            print("target is in left half of array")
             right = middle - 1
-
-        
-
+    
+    print("target not found in array")
     return -1
-
-
-binary_search_algorithm([1, 3, 5, 7, 9, 12, 44], 9)
